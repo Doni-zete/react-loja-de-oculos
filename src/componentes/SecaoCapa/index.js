addc99ebd32264bbcf5c6ad0b5679b62ec43d8d7
doni-zete <donizete112@gmail.com>
2023-12-07 07:34:55 -0300

    feat: adicionando secao da capa

---
+++ src/componentes/SecaoCapa/index.js
@@ -0,0 +1,9 @@
+export default function SecaoCapa() {
+    return (
+        <section>
+            Preços baixos e
+            Óculos de grau e de sol
+            Você só encontra aqu
+        </section>
+    )
+}
