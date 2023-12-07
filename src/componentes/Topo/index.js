d84edfdeb41643f17495ad0e9025fb5c8321dfea
doni-zete <donizete112@gmail.com>
2023-12-07 08:32:08 -0300

    feat: add seção topo

---
+++ src/componentes/Topo/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './style.css'
+
+export default function Topo() {
+ return (
+  <header>
+   <div className="secao-topo">
+    <img
+     src="assets/logo.png"
+     title="medicamento"
+     alt="imagem de um óculos à direita e o nome da ótica à esquerda"
+    />
+
+    <nav>
+     <a href="#produtos">PRODUTOS</a>
+     <a href="#sobre">SOBRE</a>
+     <a href="#contato">CONTATO</a>
+    </nav>
+   </div>
+  </header>
+ )
+}
