c0babcf6135b2527eced1a0de68cc25efcff75da
doni-zete <donizete112@gmail.com>
2023-12-07 07:31:50 -0300

    feat: rodape da pagina

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,9 @@
+export default function Rodape() {
+    return (
+        <footer>
+            <div>
+                <p>© 2023 Óticas Vida. - Todos os direitos reservados.</p>
+            </div>
+        </footer>
+    )
+}
