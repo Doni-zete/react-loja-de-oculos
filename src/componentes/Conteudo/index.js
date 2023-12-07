7bbee77b405cedb8fc3d4992d3c96f28effe5e3e
doni-zete <donizete112@gmail.com>
2023-12-07 07:29:11 -0300

    feat: import do que vai dentro do conteudo

---
+++ src/componentes/Conteudo/index.js
@@ -0,0 +1,15 @@
+import SecaoProdutos from '../SecaoProdutos';
+import SecaoContato from '../SecaoContato';
+import SecaoSobre from '../SecaoSobre';
+import SecaoCapa from '../SecaoCapa';
+
+export default function Conteudo() {
+    return (
+        <main>
+            <SecaoCapa />
+            <SecaoProdutos />
+            <SecaoSobre />
+            <SecaoContato />
+        </main>
+    )
+}
