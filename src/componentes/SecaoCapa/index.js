77a6263a21c0f97e7322d6a880d3b4149f9e8599
doni-zete <donizete112@gmail.com>
2023-12-07 08:57:19 -0300

    feat: importando css react className

--- src/componentes/SecaoCapa/index.js
+++ src/componentes/SecaoCapa/index.js
@@ -1,12 +1,14 @@
+import React from 'react'
+import './style.css'
+
 export default function SecaoCapa() {
  return (
-  <section>
+  <section className="box-imagem-de-fundo">
    <div>
-    <p>Preços baixos e</p>
-    <h1>Óculos de grau e</h1>
+    <p>Preços baixos em</p>
+    <h1>Óculos de grau e de sol</h1>
     <p>Você só encontra aqui</p>
    </div>
   </section>
  )
 }
-    
